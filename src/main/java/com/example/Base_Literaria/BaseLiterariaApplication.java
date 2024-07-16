3d3a49fd8c7d094b8548bac21b3a72e12011580d
Romero <romerodev1111@gmail.com>
2024-07-16 17:13:14 -0500

    Primer commit

---
+++ src/main/java/com/example/Base_Literaria/BaseLiterariaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Base_Literaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseLiterariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseLiterariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Base_Literaria/BaseLiterariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Base_Literaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseLiterariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
